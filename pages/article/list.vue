cf32a6d5db50d76dd5bc2399102322e784e3334c
jolin-Dudoo <marges.dudu@gmail.com>
2021-09-13 15:46:59 +0800

    part4 參數校驗

--- pages/article/list.vue
+++ pages/article/list.vue
@@ -2,9 +2,11 @@
     <div>
         <h2>Here is List</h2>
         <ol>
-            <li>Get Start with Vue</li>
-            <li>Bootstrap of Vue</li>
-            <li>Crate Test with Vue</li>
+            <li>Get Start With Nuxt</li>
+            <li>Nuxt 的基本設置</li>
+            <li>新增Nuxt 頁面</li>
+            <li>新增錯誤網頁 && 套用不同佈局</li>
+            <li>參數傳遞</li>
         </ol>
     </div>
 </template>
---
+++ pages/item/_id.vue
@@ -0,0 +1,33 @@
+
+
+<template>
+
+    <div>
+        <h2>商品內容</h2>
+        <hr>
+        <div v-if="this.$route.params.id">
+            商品ID {{this.$route.params.id}}
+        </div>
+    </div>
+    <!-- <Nuxt /> -->
+</template>
+<script>
+// import { defineComponent } from '@vue/composition-api'
+
+export default{
+    layout:'header',
+    // ===== 判斷只能輸入數字 =====
+    validate({params}){
+        // return /^\d+$/.test(params.id)
+        return  /^[0-9]*$/.test(params.id)
+    }
+}
+
+// more validation method :https://nuxtjs.org/docs/2.x/features/rendering-modes
+// export default ({
+//     validate({params}) {
+//         return /*\d+$/.test(params.id)
+        
+//     },
+// })
+</script>
