41cdcf1f92a553cf6e6dcffc74a39471e6a6fc29
jolin-Dudoo <marges.dudu@gmail.com>
2021-09-13 15:20:55 +0800

    add

---
+++ layouts/error.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="wrap_error">
+        <div class="error">
+            <h2> Error 404</h2>
+            <h2>您點選的頁面不存在</h2>
+        </div>
+        <!-- <Nuxt /> -->
+    </div>
+    
+</template>
+
+<script>
+export default {
+layout:'header_error'
+
+}
+</script>
