3a9b81d1f5ce8d2aa8bc9dc962ca66316bf0a4a7
jolin-Dudoo <marges.dudu@gmail.com>
2021-09-14 17:31:06 +0800

    no message

---
+++ components/code/codeBlock.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="functionBlock">
+        <h1>Hello myfriend!!!</h1>
+
+    </div>
+</template>
+<script>
+// import { defineComponent } from '@vue/composition-api'
+
+export default {
+    props:[],
+    data(){
+        return{
+
+    }},
+    computed:{
+
+    },
+    methods:{
+
+    },
+    
+        
+}
+</script>
---
+++ pages/button.vue
@@ -0,0 +1,43 @@
+<template>
+<div class="wrap-inside">
+  <div id="wrap-button">
+    <b-button>Button</b-button>
+    <b-button variant="danger">Button</b-button>
+    <b-button variant="success">Button</b-button>
+    <b-button variant="outline-primary">Button</b-button>
+  </div>
+   <hr>
+   <div class="codeBlock">
+     <div 
+     v-for="scriptItem,index of script" :key="index">
+     {{scriptItem.item}}<br>
+     </div>
+   </div>
+</div>
+  <!-- <Tutorial/> -->
+</template>
+
+<script>
+
+export default {
+  data(){
+    return{
+      txt:'Hello\n Test',
+      script:[
+        {item:`<b-button>Button</b-button>`},
+        {item:`<b-button variant="danger">Button</b-button>`},
+        {item:`<b-button variant="success">Button</b-button>`},
+        {item:`<b-button variant="outline-primary">Button</b-button>`}
+      ]
+    }
+  },
+  // component('codeBzlock',{})
+
+  // }
+  // methods:{
+  //   getSource(){
+  //     this.all
+  //   }
+  // }
+}
+</script>
